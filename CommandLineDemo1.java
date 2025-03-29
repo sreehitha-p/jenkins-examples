71d06c9ceeefc3b0c342ccc20e7c4b642003a1bb
sreehitha-p <sreehitha.potharla@gmail.com>
2025-03-29 12:56:04 +0530

    Add files via upload

--- CommandLineDemo1.java
+++ CommandLineDemo1.java
@@ -1,9 +1,9 @@
 class CommandLineDemo1
 {
-public static void main(String args[]);
+public static void main(String args[])
+{
 int n1=Integer.parseInt(args[0]);
 int n2=Integer.parseInt(args[1]);
-{
 int r1=n1+n2;
 int r2=n1-n2;
 int r3=n1*n2;
